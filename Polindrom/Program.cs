923f58512f2b5b53f73daa1857af5fedead77dc2
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-04-27 21:34:09 +0400

    Final

--- Polindrom/Program.cs
+++ Polindrom/Program.cs
@@ -8,30 +8,6 @@ namespace Polindrom
         {
             Console.WriteLine(CheckPolyndrom(Console.ReadLine()));
 
-            //int input = Int32.Parse(Console.ReadLine());
-            //Console.WriteLine(input);
-            // int[] arr = new int[] {4, 5, 6, 4, 7, 8, 9, 7};
-            // int count = 0;
-            // while (input > 0)
-            // {
-            //     input = input / 10;
-            //     count++;
-            // }
-            //
-            //
-            // Console.WriteLine(count);
-
-            // string inputText = Console.ReadLine();
-            // bool Flag = true;
-            //
-            // for (int i = 0; i < inputText.Length/2; i++)
-            // {
-            //     if (inputText[i]!=inputText[inputText.Length-1-i])
-            //     {
-            //         Flag = false;
-            //     }
-            // }
-
         }
 
         static bool CheckPolyndrom(string text)
